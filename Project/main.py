1e829d776f6702c4067a2105ecfab39396debb50
MehaiIulian <iulicamihaita@gmail.com>
2021-01-14 02:26:44 +0200

    Done

--- Project/main.py
+++ Project/main.py
@@ -34,71 +34,59 @@ def get_processes_info():
             # get the process id
             pid = process.pid
             if pid == 0:
-                # System Idle Process for Windows NT, useless to see anyways
                 continue
-            # print(pid)
 
             # get the name of the file executed
             name = process.name()
-            # print(name)
 
             # get the time the process was spawned
             try:
+                # spawned processes
                 create_time = datetime.fromtimestamp(process.create_time())
             except OSError:
-                # system processes, using boot time instead
+                # system processes
                 create_time = datetime.fromtimestamp(psutil.boot_time())
-            # print(create_time)
 
             try:
                 # get the number of CPU cores that can execute this process
                 cores = len(process.cpu_affinity())
             except psutil.AccessDenied:
                 cores = 0
-            # print(cores)
 
             # get the CPU usage percentage
             cpu_usage = process.cpu_percent()
-            # print(cpu_usage)
 
-            # get the status of the process (running, idle, etc.)
+            # get the status of the process
             status = process.status()
-            # print(status)
 
             try:
                 # get the process priority (a lower value means a more prioritized process)
                 nice = int(process.nice())
             except psutil.AccessDenied:
                 nice = 0
-            # print(nice)
 
             try:
                 # get the memory usage in bytes
                 memory_usage = process.memory_full_info().uss
             except psutil.AccessDenied:
                 memory_usage = 0
-            # print(memory_usage)
 
             # total process read and written bytes
             io_counters = process.io_counters()
             read_bytes = io_counters.read_bytes
             write_bytes = io_counters.write_bytes
-            # print(f'Values:{io_counters},{read_bytes},{write_bytes}')
 
             # get the number of total threads spawned by this process
             n_threads = process.num_threads()
-            # print(n_threads)
 
             # get the username of user spawned the process
             try:
                 username = process.username()
             except psutil.AccessDenied:
                 username = "N/A"
-            # print(username)
 
             # get the path of the process executable
             path = process.exe()
-            # print(path)
 
         processes.append({
             'pid': pid, 'name': name, 'path': path, 'create_time': create_time,
@@ -111,27 +99,32 @@ def get_processes_info():
 
 
 def construct_dataframe(processes):
-    # convert to pandas dataframe
     df = pd.DataFrame(processes)
-    # set the process id as index of a process
+
+    # set pid as index
     df.set_index('pid', inplace=True)
+
     # sort rows by the column passed as argument
     df.sort_values(sort_by, inplace=True, ascending=not descending)
-    # pretty printing bytes
+
+    # converting bytes in a nice format
     df['memory_usage'] = df['memory_usage'].apply(get_size)
     df['write_bytes'] = df['write_bytes'].apply(get_size)
     df['read_bytes'] = df['read_bytes'].apply(get_size)
+
     # convert to proper date format
     df['create_time'] = df['create_time'].apply(datetime.strftime, args=("%Y-%m-%d %H:%M:%S",))
-    # reorder and define used columns
+
     return df
 
 
+# convert columns string to a list
 def Convert(string):
     li = list(string.split(","))
     return li
 
 
+# process killer
 def kill_process(name):
     for proc in psutil.process_iter():
         if name in proc.name():
@@ -140,6 +133,7 @@ def kill_process(name):
             p.terminate()
 
 
+# process creator
 def create_process(name):
     try:
         os.startfile(name)
@@ -147,6 +141,7 @@ def create_process(name):
         print("Please check the name or the path again")
 
 
+# process "suspender"
 def suspend_process(pid):
     try:
         psutil.Process(pid).suspend()
@@ -156,6 +151,7 @@ def suspend_process(pid):
         pass
 
 
+# process "resumer"
 def resume_process(pid):
     try:
         psutil.Process(pid).resume()
@@ -169,10 +165,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Process Viewer & Monitor & Operations")
     parser.add_argument("--columns", help="""Columns to display,pid is set as index and available columns are 
-                                                name,create_time,cores,cpu_usage,status,nice,memory_usage,read_bytes,write_bytes,n_threads,username.
+                                                name,path,create_time,cores,cpu_usage,status,nice,memory_usage,read_bytes,write_bytes,n_threads,username.
                                                 Default is name,path.""",
                         default="name,path")
-    parser.add_argument("--name", help="Name a process name to show details about it", default="")
+    parser.add_argument("--name", help="Name a process name to show details ONLY about it", default="")
     parser.add_argument("--sort-by", dest="sort_by", help="Column to sort by, default is memory_usage .",
                         default="memory_usage")
     parser.add_argument("--descending", action="store_true", help="Whether to sort in descending order.")
@@ -191,13 +187,12 @@ if __name__ == "__main__":
     args = parser.parse_args()
     columns = args.columns
     columns = Convert(columns)
-    # print(columns)
+
     sort_by = args.sort_by
     descending = args.descending
 
     pid = args.pid
     pid = get_pid(pid)
-    # print(pid)
 
     lines = int(args.lines)
     live_update = args.live_update
